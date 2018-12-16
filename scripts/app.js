5cb92e0945a4d3ac762b4147f1182d19dabc161f
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2018-12-16 15:19:30 -0500

    added mobile responsive hamburger menu using jquery

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,23 @@ const typed = new Typed('.typedStrings', {
     strings: ["Front End Web Developer", "Sports Fanatic", "Lover of Dad Jokes", "Coffee Enthusiast"],
     typeSpeed: 60,
     backSpeed: 40,
-    showCursor: true,
+    cursor: false,
     loop: true
+});
+
+// DOCUMENT READY
+$(function () {
+    $('.hamburger').on('click', function () {
+        $('.menu').fadeIn("slow");
+    });
+    $('.exit-responsive').on('click', function () {
+        $('.menu').fadeOut("slow");
+    });
+    $('.navMobile.a').on('click', function () {
+        $('.menu').fadeOut("slow");
+    });
+    // w0w js
+    new WOW().init();
+    // smooth scroll
+    $('nav ul li a').smoothScroll();
 });
