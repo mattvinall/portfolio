2e7bf5227136cb4eb57ce806f477736ed1015573
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2018-12-16 11:49:20 -0500

    added animate.css, wow.js and fixed type.js issue

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// header
+const typed = new Typed('.typedStrings', {
+    strings: ["Front End Web Developer", "Sports Fanatic", "Lover of Dad Jokes", "Coffee Enthusiast"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    showCursor: true,
+    loop: true
+});
