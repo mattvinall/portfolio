0713075e54815d284f9e9a916a43df29d4dd2b38
MattVinall <matt.vinall7@gmail.com>
2019-06-28 00:28:03 -0400

    added two projects, added appropriate styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,33 @@
 // header
 const typed = new Typed('.typedStrings', {
-    strings: ["Front End Web Developer", "Sports Fanatic", "Lover of Dad Jokes", "Coffee Enthusiast"],
-    typeSpeed: 60,
-    backSpeed: 40,
-    cursor: false,
-    loop: true
+	strings: [ 'Front End Web Developer', 'Sports Fanatic', 'Lover of Dad Jokes', 'Coffee Enthusiast' ],
+	typeSpeed: 60,
+	backSpeed: 40,
+	cursor: false,
+	loop: true
 });
 
 // DOCUMENT READY
-$(function () {
-    $('.hamburger').on('click', function () {
-        $('.menu').fadeIn("slow");
-    });
-    $('.exit-responsive').on('click', function () {
-        $('.menu').fadeOut("slow");
-    });
-    $('.navMobile.a').on('click', function () {
-        $('.menu').fadeOut("slow");
-    });
-    // w0w js
-    new WOW().init({
-        mobile: false,
-        offset: 50
-    });
-    // smooth scroll
-    $('nav ul li a').smoothScroll({
-        speed: 700
-    });
-    $('.smooth-scroll').smoothScroll({
-        speed: 700
-    });
-
+$(function() {
+	$('.hamburger').on('click', function() {
+		$('.menu').fadeIn('slow');
+	});
+	$('.exit-responsive').on('click', function() {
+		$('.menu').fadeOut('slow');
+	});
+	$('.navMobile.a').on('click', function() {
+		$('.menu').fadeOut('slow');
+	});
+	// w0w js
+	new WOW().init({
+		mobile: false,
+		offset: 50
+	});
+	// smooth scroll
+	$('nav ul li a').smoothScroll({
+		speed: 700
+	});
+	$('.smooth-scroll').smoothScroll({
+		speed: 700
+	});
 });
